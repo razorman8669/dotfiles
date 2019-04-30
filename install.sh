f685c23a3ea88d41918d076011add606128a88d4
Ray Marceau <ray@raymarceau.com>
2019-04-30 11:57:17 -0700

    updates

--- install.sh
+++ install.sh
@@ -36,6 +36,9 @@ ln -s $HOME/.dotfiles/.gitignore_global ~/.gitignore_global
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# Set line endings: https://help.github.com/en/articles/dealing-with-line-endings
+git config --global core.autocrlf input
+
 # Set macOS preferences
 # We will run this last because this will reload the shell
 source .macos
